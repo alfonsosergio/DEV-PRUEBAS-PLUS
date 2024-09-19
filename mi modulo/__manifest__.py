2eefde49d3dbc2e5961800bf3a002f0c0e79c597
alfvip <salfonso@vippinn.com>
2024-09-19 11:51:29 -0300

    modulo nuevo

---
+++ mi modulo/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Mi Primer Módulo',
+    'version': '17.0',
+    'author': 'Tu Nombre',
+    'category': 'Uncategorized',
+    'depends': ['base'],
+    'data': [
+        'views/mi_vista.xml',
+        'security/ir.model.access.csv',
+    ],
+    'installable': True,
+    'application': True,
+}
